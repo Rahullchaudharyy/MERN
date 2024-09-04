89357bedd0b41cfd9fdb0645e9b9a66d241c0a47
Rahullchaudharyy <rahullchaudharryy@gmail.com>
2024-09-04 10:37:39 +0530

    BaCKEND..fRAUTH

--- Movie-App/frontend/src/main.jsx
+++ Movie-App/frontend/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import store from "./redux/store.js";
+import { Provider } from "react-redux";
+import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// auth
+
+// ristricted user
+
+const route = createBrowserRouter(
+  createRoutesFromElements(<Route path="/" element={<App />}></Route>)
+);
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <RouterProvider router={route}>
+      <App />
+    </RouterProvider>
+  </Provider>
+);
---
+++ Movie-App/frontend/src/redux/store.js
@@ -0,0 +1,21 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {setupListener} from '@reduxjs/toolkit/query/react';
+import authReducer from './features/auth/authSlice'
+
+const store = configureStore({
+    
+
+    reducer:{
+        auth:authReducer,
+    },
+    middleware:(getDefaultMiddleware) = getDefaultMiddleware().concat(apiSlice.middleware),
+    devTools:true,
+})
+
+
+
+
+setupListener(store.dispatch)
+
+
+export default store;
